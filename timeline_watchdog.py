a3958f8a9f440271fc1d8bc2eaafffbcda656c4a
hiperbolt <hiperboltz@gmail.com>
2019-07-25 00:43:16 +0100

    commit 2

--- timeline_watchdog.py
+++ timeline_watchdog.py
@@ -14,11 +14,15 @@ port = 9993
 def main(timeline):
     #ethernetprotocolapi.play(ip, port) dar play ao primeiro da timeline depois ficar a espera que ele acabe e so on e so forth
     loadedClips = ethernetprotocolapi.diskList(ip, port).decode().splitlines()[6:][:-1]
-    #for clip in loadedClips
+    if set(eval(timeline)).issubset(loadedClips) == True: #DANGEROUS!! TRUSTING INPUT TO BE CORRECT!! IF THE .KTMF FILE IS CORRUPT SHIT WILL HAPPEN
+        #This means the timeline is valid
+        for range(1, len(eval(timeline))) as x:
+            #descobrir qual a subtstring de loaded clips que = a timeline[x] e dar play a esse e dormir por a duração
 
     loadedClipId = ethernetprotocolapi.transportInfo(ip, port).decode().splitlines()[8][9:]
     #timecode = ethernetprotocolapi.clipsGetId(ip, port, int(clipId)).decode("utf-8").splitlines()[6][-11:] #get currently playing clip's duration
     # Traduzir time code para segundos e time.sleep esses segundos e repetir o loop, sys.exit quando acabar
+    print(timeline)
     print(loadedClips)
     print(loadedClipId)
     #ethernetprotocolapi.play(ip, port)
